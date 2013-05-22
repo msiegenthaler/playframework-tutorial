3a60bad5b1ddc707210e9bbb18387395cdc57bc7
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-05-22 22:18:22 +0200

    bugfix for postgres dep.

--- project/Build.scala
+++ project/Build.scala
@@ -10,15 +10,12 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "postgresql" % "postgresql" % "8.4-702.jdbc4"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
-  )
-
-  val appDependencies = Seq(
-    "postgresql" % "postgresql" % "8.4-702.jdbc4"
   )
 }
